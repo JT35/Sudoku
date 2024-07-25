478352c3787c184cea62dc128f6c9968e95ceab8
Joshua Thompson <jdtuutp45@gmail.com>
2024-07-25 16:07:04 -0400

    Properly renamed files from default names

--- src/main/java/com/example/sudoku/MainController.java
+++ src/main/java/com/example/sudoku/MainController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
